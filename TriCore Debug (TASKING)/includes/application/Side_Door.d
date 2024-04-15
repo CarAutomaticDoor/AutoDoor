1202b3d35ce7a7ff062042982dcbb6b20a0847d1
ChaeYes <80445991+kche1106@users.noreply.github.com>
2024-04-15 20:00:03 +0900

    auto door function implementation complete

--- TriCore Debug (TASKING)/includes/application/Side_Door.d
+++ TriCore Debug (TASKING)/includes/application/Side_Door.d
@@ -2,9 +2,9 @@ includes/application/Side_Door.o :	../includes/application/Side_Door.c
 ../includes/application/Side_Door.c :
 includes/application/Side_Door.o :	../includes/application/Side_Door.h
 ../includes/application/Side_Door.h :
-includes/application/Side_Door.o :	c:/git-repo/AutoDoor/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Platform_Types.h
-c:/git-repo/AutoDoor/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Platform_Types.h :
-includes/application/Side_Door.o :	c:/git-repo/AutoDoor/includes/general/Servo.h
-c:/git-repo/AutoDoor/includes/general/Servo.h :
-includes/application/Side_Door.o :	c:/git-repo/AutoDoor/includes/general/Delay.h
-c:/git-repo/AutoDoor/includes/general/Delay.h :
+includes/application/Side_Door.o :	c:/Project1/AutoDoor/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Platform_Types.h
+c:/Project1/AutoDoor/Libraries/iLLD/TC27D/Tricore/Cpu/Std/Platform_Types.h :
+includes/application/Side_Door.o :	c:/Project1/AutoDoor/includes/general/Servo.h
+c:/Project1/AutoDoor/includes/general/Servo.h :
+includes/application/Side_Door.o :	c:/Project1/AutoDoor/includes/general/Delay.h
+c:/Project1/AutoDoor/includes/general/Delay.h :
